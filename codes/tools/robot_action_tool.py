465741b0034e29c3bdcba179b6703a3eb0d9a3fe
zhouxy <150273112@qq.com>
2024-09-19 10:54:00 +0800

    修改自定义的tool

--- codes/tools/robot_action_tool.py
+++ codes/tools/robot_action_tool.py
@@ -1,11 +1,12 @@
 #tools as a search tool， search the api and return the result
 
-from langchain_core import tools
+from langchain_core.tools import tool
 from codes.schema.robot_api import RobotActionDescription
+from typing import Annotated, List
 
-@tools
-def robot_action(action: str) -> RobotActionDescription:
+@tool
+def robot_action(action: Annotated[str, ""] ) -> RobotActionDescription:
     return RobotActionDescription(
         function_name="robot_action",
-        parameters={"action": action}
+        parameters={}
     )    
